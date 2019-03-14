b5033125fea0f350afe6f66830bf5dfbb7cc36d0
Gabriel <utenkar@gmail.com>
2019-03-14 19:19:33 +0100

    ccc

--- Gui.py
+++ Gui.py
@@ -21,14 +21,16 @@ class LoginFrame(Frame):
         self.entry_username = Entry(self)
         self.entry_password = Entry(self, show="*")
         #string var za spreminjajoc se label
-        self.besedilo= StringVar(self)
-        self.update_label=Label(self, textvariable=self.besedilo)
+        self.besedilo = StringVar(self)
+        self.update_label = Label(self, textvariable=self.besedilo)
 
         self.label_username.grid(row=0, sticky=E)
         self.label_password.grid(row=1, sticky=E)
         self.entry_username.grid(row=0, column=1)
         self.entry_password.grid(row=1, column=1)
         self.update_label.grid(row=3, sticky=E)
+        #da fokusira okence
+        self.entry_username.focus()
 
         self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
         self.logbtn.grid(row=2, column=1)
@@ -37,9 +39,6 @@ class LoginFrame(Frame):
         self.regbtn.grid(row=3, column=1)
 
         self.pack()
-
-
-
 
     def _register_btn_clicked(self):
 
@@ -67,17 +66,28 @@ class LoginFrame(Frame):
         self.newWindow = Toplevel(self.master)
         MainScreen(self.newWindow)
 
+
 class MainScreen(Frame):
     def __init__(self, master):
         super().__init__(master)
+
+        self.master.title("My work h app")
+        self.master.geometry("550x250")
+        self.master.iconbitmap(r"favicon.ico")
+        self.master.configure(background='#22272d')
 
         self.frame = Frame(self.master)
         self.quitButton = Button(self.frame, text='Quit', width=25, command=self.close_windows)
         self.quitButton.pack()
         self.frame.pack()
 
+    def close_windows(self):
+        print("nice")
+
+
 
 if __name__ == '__main__':
     root = Tk()
     prvo_okno = LoginFrame(root)
+
     root.mainloop()
